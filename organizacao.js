eb0d8ee04cb2b1a517adbfbe809204a1ebfc8b98
Cristian Pessanha da Silva <cristian.silva@ccgl.intra>
2021-10-05 16:21:24 -0300

    Organização Básica de um Código

---
+++ organizacao.js
@@ -0,0 +1,16 @@
+/**
+ * Sendo especifico,
+ *  a linguagem é organizada em sentença de código e em blocos de código
+ *  e esses blocos de códigos são agrupamentos de linhas de código ou sentenças..
+ */
+
+//sentença de código
+console.log("Sentença de código")
+
+// blocos de código
+{
+    {
+        console.log("Olá");
+        console.log("Mundo!")
+    }
+}
