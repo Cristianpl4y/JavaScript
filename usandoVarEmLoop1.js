0bea84f4297ea443a25234afee6567aebfe2efed
Cristian Pessanha da Silva <cristian.silva@ccgl.intra>
2021-10-07 10:29:50 -0300

    Usando Var em Loop 01

---
+++ usandoVarEmLoop1.js
@@ -0,0 +1,10 @@
+/* A instrução for cria um loop que consiste em três expressões opcionais, dentro de parênteses e separadas por ponto e vírgula, 
+seguidas por uma declaração ou uma sequência de declarações executadas em sequência. */
+
+for (var i = 0; i < 10; i++){
+    console.log(i)
+}
+
+console.log('i =', i)
+
+// Laços oferecem um jeito fácil e rápido de executar uma ação repetidas vezes.
