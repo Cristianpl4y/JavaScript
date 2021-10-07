50ea2d0575329ffe5819d534e309fc9f6c5ff8a1
Cristian Pessanha da Silva <cristian.silva@ccgl.intra>
2021-10-07 08:50:28 -0300

    Declaração de Variáveis Com Var

---
+++ usandoVar1.js
@@ -0,0 +1,25 @@
+{
+    {
+        {
+            {
+                var sera = 'Será???'
+                console.log(sera)
+            }
+        }
+    }
+}
+console.log(sera) // funciona
+
+function teste() {
+    var local = 123
+    console.log(local)
+}
+
+teste()
+//console.log(local) // não funciona 
+
+/* 
+    var Descrição. Declarações de variáveis, onde quer que elas ocorram, são processadas antes que qualquer outro código seja executado.
+    O escopo de uma variável declarada com var é seu contexto atual em execução, o qual é a função a qual pertence ou,
+    para variáveis declaradas fora de qualquer função, o escopo é o global. 
+*/
