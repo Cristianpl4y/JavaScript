4b21f92869a9235219fd86ecc95e4eb2c6ccf2d3
Cristian Pessanha da Silva <cristian.silva@ccgl.intra>
2021-10-07 10:36:46 -0300

    Usando Let em Loop 01

---
+++ usandoLetEmLoop.js
@@ -0,0 +1,7 @@
+for (let i = 0; i < 10; i++){
+    console.log(i)
+}
+
+console.log('i =', i)
+
+// Usando let, em um laço for a variavel i não vai estar definada fora do bloco de codigo pois o let tem o escopo de bloco de codigo.
