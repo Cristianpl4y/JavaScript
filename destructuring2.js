711b2f8c2dfe15fbba4d907811f4255db3213b6e
Cristian Pessanha da Silva <cristian.silva@ccgl.intra>
2021-10-14 09:18:15 -0300

    Operadores: Destructuring 2

---
+++ destructuring2.js
@@ -0,0 +1,11 @@
+// Desestruturação de array usando []
+
+const [a] = [10]
+console.log(a)
+
+const [n1, , n3, , n5, n6 = 0] = [10, 7, 9, 8]
+console.log(n1, n3, n5, n6)
+
+
+const [, [, nota]] = [[, 8,8], [9, 6, 8]] // só como exemplo não é uma boa pratica!
+console.log(nota)
