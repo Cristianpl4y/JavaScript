44315669690e3f45e9e7822087c0a550f087e9ac
Cristian Pessanha da Silva <cristian.silva@ccgl.intra>
2022-02-15 17:44:26 -0300

    add controle

---
+++ controle/ifElse.js
@@ -0,0 +1,10 @@
+const imprimirResult = function(nota){
+    if(nota >= 7){
+        console.log('Você passou!');
+    }else{
+        console.log('Você reprovou!');
+    }
+}
+
+imprimirResult(10)
+imprimirResult(6)
