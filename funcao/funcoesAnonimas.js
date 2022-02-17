8fc2654a0159e4389ec7106ecdc250ac2aed7a96
Cristian Pessanha da Silva <cristian.silva@ccgl.intra>
2022-02-17 08:44:12 -0300

    add função anonima

---
+++ funcao/funcoesAnonimas.js
@@ -0,0 +1,27 @@
+const soma = function (x, y){
+    return x + y
+}
+
+const imprimirResultado = function (a, b, operacao = soma){
+    console.log(operacao(a, b))
+}
+
+imprimirResultado(3, 4)
+imprimirResultado(3, 4, soma)
+imprimirResultado(3, 4, function (x, y){
+   return x - y
+})
+
+imprimirResultado(3, 4, (x, y) => x * y)
+
+const pessoa = {
+    falar : function () { console.log( 'Oi!' ) }
+}
+
+pessoa.falar()
+
+const pessoafalar2 = {
+    falar : () => 'Opa!'
+}
+
+console.log(pessoafalar2.falar())
