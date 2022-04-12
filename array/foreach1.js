c46dd712e11519b6429b61fcdb3c9d4fd97b7569
Cristian Pessanha da Silva <cristian.silva@ccgl.intra>
2022-04-12 13:55:56 -0300

    estudando array

---
+++ array/foreach1.js
@@ -0,0 +1,10 @@
+const aprovados = ['Bia', 'Leo', 'Giovana', 'Pablo', 'Maria']
+
+aprovados.forEach(function(nome, indice){
+    console.log(`${indice + 1}) ${nome}`)
+})
+
+aprovados.forEach(nome => console.log(nome))
+
+const exibirAprovados = aprovado => console.log(aprovado)
+aprovados.forEach(exibirAprovados)
